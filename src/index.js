e7c04da227e651422f6ab4b8cf9ce465b59dab39
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-09-01 13:59:40 +0200

    Getting Started with React Redux

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
+
+//Getting Started with React Redux
+class DisplayMessages extends React.Component {
+  // change code below this line
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      messages: []
+    }
+  }
+  // change code above this line
+  render() {
+    return <div />
+  }
+};

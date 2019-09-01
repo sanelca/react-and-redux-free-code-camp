753310a8c422d3818cbefab8c9016edca40f0062
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-09-01 14:21:39 +0200

    Manage State Locally First

--- src/index.js
+++ src/index.js
@@ -18,3 +18,46 @@ class DisplayMessages extends React.Component {
     return <div />
   }
 };
+
+//Manage State Locally First
+class DisplayMessages2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.submitMessage = this.submitMessage.bind(this)
+  }
+  // add handleChange() and submitMessage() methods here
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  submitMessage() {
+    let newMessages = [...this.state.messages, this.state.input]
+    this.setState({
+     messages: newMessages,
+     input: ''
+    })
+  }
+
+  render() {
+      return (
+        <div>
+          <h2>Type in a new Message:</h2>
+          { /* render an input, button, and ul here */ }
+          <input onChange={this.handleChange} value={this.state.input}/>
+          <button onClick={this.submitMessage}>submit</button>
+          <ul>
+            {this.state.messages.map(msg => <li>{msg}</li>)}
+          </ul>
+          { /* change code above this line */ }
+        </div>
+      );
+    }
+};
+ReactDOM.render(<DisplayMessages2 />, document.getElementById('root'))
